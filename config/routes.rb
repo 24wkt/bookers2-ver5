e9a0601bd2ee1f74ba01031729d574453cc8c9a0
24wkt <05wkt38xy.waku@gmail.com>
2021-04-01 23:11:11 +0000

    いいねの途中

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
+
   resources :books do
-    resources :users,only: [:show,:index,:edit,:update]
-    resource :favorites,only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show,:index,:edit,:update]
+
+
 end
