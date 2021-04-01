030d173a0e29a9765fa6ca98f2434e209bb003d7
24wkt <05wkt38xy.waku@gmail.com>
2021-04-01 22:10:56 +0000

    favorire_model作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resources :users,only: [:show,:index,:edit,:update]
+    resource :favorites,only: [:create, :destroy]
+  end
 end
