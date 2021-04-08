aafa5d8133f2045fe587f491c9cec57644678788
24wkt <05wkt38xy.waku@gmail.com>
2021-04-08 08:43:36 +0000

    ajax.

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,18 +5,13 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
-    if @book_comment.save
-      redirect_to book_path(@book.id)
-    else
-      render 'books/show'
-    end
+    @book_comment.save
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    book_comment = @book.book_comments.find_by(book_id: @book.id)
-    book_comment.destroy
-    redirect_to request.referer
+    @book_comment = @book.book_comments.find_by(book_id: @book.id)
+    @book_comment.destroy
   end
 
   private
